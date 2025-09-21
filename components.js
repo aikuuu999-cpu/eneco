3964024594aeba0863a3aec69fed018813fbf065
aikuuu999 <aikuuu999@gmail.com>
2025-09-21 17:55:24 +1000

    1

--- components.js
+++ components.js
@@ -7,32 +7,6 @@ const SiteComponents = {
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="styles.css">
-    
-    <!-- Yandex.Metrika debug script -->
-    <script src="https://mc.yandex.ru/metrika/tag_debug.js?ver=2204&b=" type="text/javascript" charset="utf-8" async=""></script>
-    
-    <!-- Yandex.Metrika counter -->
-    <script type="text/javascript">
-        (function (m, e, t, r, i, k, a) {
-            m[i] = m[i] || function () {
-                (m[i].a = m[i].a || []).push(arguments)
-            };
-            m[i].l = 1 * new Date();
-            for (var j = 0; j < document.scripts.length; j++) {
-                if (document.scripts[j].src === r) {
-                    return;
-                }
-            }
-            k = e.createElement(t), a = e.getElementsByTagName(t)[0], k.async = 1, k.src = r, a.parentNode.insertBefore(k, a)
-        })(window, document, 'script', 'https://mc.yandex.ru/metrika/tag.js?id=104214599', 'ym');
-        ym(104214599, 'init', {ssr: true, webvisor: true, clickmap: true, accurateTrackBounce: true, trackLinks: true});
-    </script>
-    <noscript>
-        <div>
-            <img src="https://mc.yandex.ru/watch/104214599" style="position:absolute; left:-9999px;" alt=""/>
-        </div>
-    </noscript>
-    <!-- /Yandex.Metrika counter -->
 `,
 
     header: `
@@ -152,12 +126,6 @@ const SiteComponents = {
     </footer>`
 };
 
-// Функция инициализации Яндекс.Метрики (теперь только проверяет готовность)
-function initYandexMetrika() {
-    // Яндекс.Метрика теперь загружается из head секции
-    // Эта функция оставлена для совместимости и дополнительных проверок
-    console.log('Yandex.Metrika загружена из head секции');
-}
 
 // Page loader management
 const PageLoader = {
@@ -245,11 +213,9 @@ function detectWebPSupport() {
 if (document.readyState === 'loading') {
     document.addEventListener('DOMContentLoaded', function() {
         loadSiteComponents();
-        initYandexMetrika();
         detectWebPSupport();
     });
 } else {
     loadSiteComponents();
-    initYandexMetrika();
     detectWebPSupport();
 }

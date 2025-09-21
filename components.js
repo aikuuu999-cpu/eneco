6417472fa1525acd043520c6ed8c87f7b9792916
aikuuu999 <aikuuu999@gmail.com>
2025-09-21 23:15:39 +1000

    ya

--- components.js
+++ components.js
@@ -219,37 +219,6 @@ function loadSiteComponents() {
             headElement.appendChild(styleLink);
         }
         
-        // Добавляем Яндекс.Метрику
-        const hasYandexMetrica = headElement.querySelector('script[src*="mc.yandex.ru"]');
-        if (!hasYandexMetrica) {
-            // Добавляем скрипт Яндекс.Метрики
-            const ymScript = document.createElement('script');
-            ymScript.type = 'text/javascript';
-            ymScript.innerHTML = `
-(function(m,e,t,r,i,k,a){
-    m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-    m[i].l=1*new Date();
-    for (var j = 0; j < document.scripts.length; j++) {
-        if (document.scripts[j].src === r) { return; }
-    }
-    k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
-})(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=104214599', 'ym');
-
-ym(104214599, 'init', {
-    ssr:true, 
-    webvisor:true, 
-    clickmap:true, 
-    accurateTrackBounce:true, 
-    trackLinks:true
-});`;
-            headElement.appendChild(ymScript);
-            
-            // Добавляем noscript блок
-            const noscriptBlock = document.createElement('noscript');
-            noscriptBlock.innerHTML = '<div><img src="https://mc.yandex.ru/watch/104214599" style="position:absolute; left:-9999px;" alt="" /></div>';
-            headElement.appendChild(noscriptBlock);
-        }
-        
         // Отмечаем, что компоненты загружены
         headElement.setAttribute('data-components-loaded', 'true');
     }

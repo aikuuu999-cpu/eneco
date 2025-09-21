856a8fca2c678984666b4c05bb041c480447a4f8
aikuuu999 <aikuuu999@gmail.com>
2025-09-21 22:49:34 +1000

    1

--- components.js
+++ components.js
@@ -10,42 +10,6 @@ const SiteComponents = {
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="styles.css">
-<!-- Yandex.Metrika counter -->
-<script type="text/javascript">
-    (function(m, e, t, r, i, k, a) {
-        m[i] = m[i] || function() {
-            (m[i].a = m[i].a || []).push(arguments)
-        };
-        m[i].l = 1 * new Date();
-        for (var j = 0; j < document.scripts.length; j++) {
-            if (document.scripts[j].src === r) {
-                return;
-            }
-        }
-        k = e.createElement(t);
-        a = e.getElementsByTagName(t)[0];
-        k.async = 1;
-        k.src = r;
-        a.parentNode.insertBefore(k, a);
-    })(window, document, 'script', 'https://mc.yandex.ru/metrika/tag.js?id=104214599', 'ym');
-
-    ym(104214599, 'init', {
-        ssr: true,
-        webvisor: true,
-        clickmap: true,
-        accurateTrackBounce: true,
-        trackLinks: true
-    });
-</script>
-<noscript>
-    <div>
-        <img src="https://mc.yandex.ru/watch/104214599" 
-             style="position:absolute; left:-9999px;" 
-             alt="" />
-    </div>
-</noscript>
-<!-- /Yandex.Metrika counter -->
-
 `,
 
     header: `
@@ -221,6 +185,23 @@ function loadSiteComponents() {
             headElement.appendChild(tempDiv.firstChild);
         }
     }
+    
+    // Яндекс.Метрика - выполняем программно
+    const ymScript = document.createElement('script');
+    ymScript.innerHTML = `
+(function(m,e,t,r,i,k,a){
+  m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+  m[i].l=1*new Date();
+  k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+})(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+ym(104214599, "init", {
+  clickmap:true,
+  trackLinks:true,
+  accurateTrackBounce:true
+});
+`;
+    document.head.appendChild(ymScript);
     
     // Загружаем header
     const headerPlaceholder = document.getElementById('header-placeholder');
